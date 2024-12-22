b2290eb802b93fb3a275920183db52fdadfb6d41
SamuelMaged90 <SamuelMaged90@gmail.com>
2024-12-22 03:02:12 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,250 @@
+﻿using System.Xml;
+
+namespace Demo
+{
+    //1- Class 
+    //2- Struct 
+    //3- interface 
+    //4- Enum
+    internal class Program
+    {
+        /// Note the default should be in the last in parameters
+        public static void PrintShape(int count , string shape ="=) ")
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(shape);
+            }
+        }
+        public static void Swap_By_Value(int x,int y)
+        {
+            int temp = x; 
+            x = y; 
+            y = temp;
+        }
+        public static void Swap_By_Ref(ref int x,ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        public static int Sum_Array_By_Value(int[] arr)
+        {
+            int sum = 0; 
+            if (arr is not null)
+            {
+                arr[0] = 100;
+                for(int i = 0;i < arr.Length;i++)
+                {
+                    sum += arr[i];
+                }
+            }
+            return sum;
+        }
+        public static int Sum_Array_By_Ref(ref int[] arr)
+        {
+            int sum = 0;
+            if (arr is not null)
+            {
+                arr[0] = 100;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    sum += arr[i];
+                }
+            }
+            return sum;
+        }
+
+        public static void Sum_Out(int x ,int y,out int sum , out int sub )
+        {
+            sum = x +y;
+            sub = x - y;
+        }
+
+        public static int SumOfArray(params int[] arr)
+        {
+            int sum = 0;
+            if(arr is not null)
+            {
+                for(int i = 0;i<arr.Length ; i++)
+                {
+                    sum+= arr[i];
+                }
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            #region Array Methods
+            #region Class Member Methods
+            //int[] numbers = { 2, 5, 6, 5, 1 };
+            //Array.Sort(numbers);
+
+            //Array.Reverse(numbers);
+
+            //Array.Clear(numbers);
+
+            //Array.Clear(numbers, 0, 2);
+
+            //int index = Array.IndexOf(numbers, 5);
+            //Console.WriteLine(index);
+
+            //Console.WriteLine(Array.LastIndexOf(numbers,5));
+
+            //Array.CreateInstance(typeof(int), 6); 
+
+            //Console.WriteLine(numbers.GetHashCode());
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Array.Resize(ref numbers, 7);
+            //Console.WriteLine(numbers.GetHashCode());
+            //foreach(int i in numbers)   
+            //    { 
+            //    Console.WriteLine(i);
+            //}   
+
+            //int[] arr01 = new int[] { 1, 2, 3, 4, 5,6};
+            //int[] arr02 = new int[7];
+
+            //Array.Copy(arr01, arr02, 2);
+            //foreach (int i in arr02)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Array.Copy(arr01, 2, arr02, 2, 3);
+            //foreach (int i in arr02)
+            //{
+            //    Console.WriteLine(i);
+            //} 
+            #endregion
+            #region Object Member Methods
+            //int[] arr = { 1, 2, 3, 4 };
+            //Console.WriteLine(arr.GetLength(0));
+            //Console.WriteLine(arr.Length);
+            //Console.WriteLine(arr.GetValue(1));
+            //arr.SetValue(100,0);
+            //Console.WriteLine(arr[0]);
+            #endregion
+
+
+
+
+
+
+
+            #endregion
+
+            #region 2D Array Using 1 For Loop
+
+            //int[,] arr = new int[2, 3];
+            //for (int i = 0; i < arr.Length; )
+            //{
+            //    Console.WriteLine($"Row {i / arr.GetLength(1)} Column {i%arr.GetLength(1)}");
+            //    bool parsed = int.TryParse( Console.ReadLine(), out arr[i / arr.GetLength(1), i % arr.GetLength(1)] );
+            //    if (parsed)
+            //    {
+            //        i++;
+            //    }
+            //    else
+            //    {
+            //        i = i; 
+            //    }
+            //}
+            //Console.Clear();
+
+            //for (int i = 0;i < arr.Length;i++)
+            //{
+            //    Console.WriteLine(arr[i / arr.GetLength(1), i % arr.GetLength(1)]);
+            //}
+            #endregion
+
+            #region Fucntions
+
+            #region ProtoType
+            //PrintShape(10,"S");
+            //PrintShape();
+            //PrintShape(shape: "G");
+            //PrintShape(5);
+
+            //Console.WriteLine("/*\\");
+            //Console.WriteLine("Hello\tWorld");
+            //Console.WriteLine("Hello\rSaSa");
+            //Console.WriteLine("Hello\bWorld");
+            //Console.WriteLine("Hello 'World ");
+            //Console.WriteLine("Hello \"World ");
+            //Console.WriteLine(@"Sasa \ Maged \ Sasa");
+
+            //int num = 123456;
+            //Console.WriteLine($"My num is {{{num}}} ");
+
+
+            #endregion
+
+            #region Value Type Parameter [Passing By Value]
+            //int a = 10;
+            //int b = 20;
+            //Console.WriteLine($"a = {a} b = {b}");
+            //Swap_By_Value(a, b);
+            //Console.WriteLine($"a = {a} b = {b}");
+
+            #endregion
+            #region Value Type Parameter [ Passing By Ref ]
+            //int a = 10;
+            //int b = 20;
+            //Console.WriteLine($"a = {a} B = {b}");
+            //Swap_By_Ref(ref a, ref b);
+            //Console.WriteLine($"a = {a} B = {b}");
+
+            #endregion
+
+            #region Reference Type Parameter [Passing By Value]
+            //int[] numbers = new int[3] {1,2,3};
+            //Console.WriteLine(numbers[0]);
+            //int result = Sum_Array_By_Value(numbers);
+            //Console.WriteLine(result);
+            //Console.WriteLine(numbers[0]);
+            #endregion
+            #region Reference Type Parameter [Passing By Value]
+            //int[] numbers = new int[3] { 1, 2, 3 };
+            //Console.WriteLine(numbers[0]);
+            //int result = Sum_Array_By_Ref(ref numbers);
+            //Console.WriteLine(result);
+            //Console.WriteLine(numbers[0]);
+            #endregion
+
+            #region out parameter 
+            //int x = 10 , 
+            //    y = 10;
+
+            //int sum ;
+            //int sub ;
+            //Sum_Out(x, y,out sum,out sub);
+            //Console.WriteLine(sum);
+            //Console.WriteLine(sub);
+            #endregion
+
+            #region Params
+            //int x = 10;
+            //int y = 10;
+            //int z = 10;
+            //int d = 10;
+
+            //Console.WriteLine(SumOfArray(x,y,d,z));
+            #endregion
+
+            object x = 123456;
+            Console.WriteLine(x);
+            Console.WriteLine(x.GetHashCode());
+            x = "Sasa";
+            Console.WriteLine(x);
+            Console.WriteLine(x.GetHashCode());
+            x = 'A';
+            Console.WriteLine(x);
+            Console.WriteLine(x.GetHashCode());
+
+            #endregion
+        }
+    }
+}
---
+++ Task05/Program.cs
@@ -0,0 +1,181 @@
+﻿namespace Task05
+{
+    internal class Program
+    {
+
+        static void PassByValue(int x)
+        {
+            x++; 
+        }
+        static void PassByRef(ref int x)
+        {
+            x++;
+        }
+        static void RefPassByValue(int[] arr)
+        {
+            if (arr is not null)
+            {
+                arr[0] = 50;
+            }
+        }
+        static void RefPassByRef(ref int[] arr)
+        {
+            if (arr is not null)
+            {
+                arr[0] = 50;
+            }
+        }
+        static void Sum_Sub(int x,int y ,out int sum , out int sub )
+        {
+            sum = x + y;
+            sub = x - y;
+        }
+        static int SumOfDigits(int num)
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                int digit = num % 10;
+                sum += digit;
+                num = num / 10;
+            }
+            return sum;
+        }
+        static bool IsPrime(int number)
+        {
+            // 0 and 1 are not prime numbers -  Even numbers greater than 2 are not prime
+            if (number <= 1 || number % 2 == 0) 
+                return false; 
+            if (number == 2) 
+                return true; // 2 is a prime number
+           
+            for (int i = 3; i <= Math.Sqrt(number); i += 2) // Check odd divisors up to the square root
+            {
+                if (number % i == 0)
+                    return false; 
+            }
+
+            return true; 
+        }
+        static void MinMaxArray(ref int[] arr, ref int min , ref int max)
+        {
+            if (arr is not null)
+            {
+                min = arr[0];
+                max = arr[0];
+                for(int i = 1; i < arr.Length; i++)
+                {
+                    if(arr[i] < min)
+                    {
+                        min = arr[i];
+                    }
+                    if(arr[i] > max)
+                    {
+                        max = arr[i];
+                    }
+                }
+            }
+        }
+        static long Factorial(int number)
+        {
+            long result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+        static string ChangeChar(string input, int position, char newChar)
+        {
+            if (input is not null && input.Length > position)
+            {
+                char[] charArray = input.ToCharArray();
+
+                charArray[position] = newChar;
+
+                string output = new string(charArray);
+
+                return output;
+            }
+            else
+            {
+                string output = "Not Valid";
+                return output;
+            }
+        }
+        static void Main(string[] args)
+        {
+            #region 1-Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example 
+            /*
+             * Passing by Value :
+             * When a value type parameter is passed by value, a copy of the variable's value is made and used inside the method. Modifications to the parameter inside the method do not affect the original variable.
+             * Passing by Reference : 
+             * When a value type parameter is passed by reference, the method works with the actual variable rather than a copy. Any changes made to the parameter inside the method will directly affect the original variable.
+             */
+            //int b = 5;
+            //Console.WriteLine(b);
+            //PassByValue(b);
+            //Console.WriteLine(b);
+            //PassByRef(ref b);
+            //Console.WriteLine(b);
+            #endregion
+
+            #region 2-Explain the difference between passing (Reference type parameters) by value and by reference then write a suitable c# example.
+            /*
+             * Passing Reference Type Parameters by Value : 
+             * When a reference type parameter is passed by value, a copy of the reference to the object is passed. While the reference itself cannot be changed, 
+             * the underlying object it refers to can be modified. Changes made to the object's properties or fields inside the method will affect the original object.
+             * Passing Reference Type Parameters by Reference : 
+             * When a reference type parameter is passed by reference (using the ref keyword), the actual reference itself is passed. This allows the method to reassign the reference to point to a different object, 
+             * in addition to modifying the original object's properties or fields.
+             */
+            //int[] arr = { 100, 50};
+            //Console.WriteLine(arr[0]);
+            //RefPassByValue(arr);
+            //Console.WriteLine(arr[0]);
+            //RefPassByRef(ref arr);
+            //Console.WriteLine(arr[0]);
+
+            #endregion
+
+            #region 3-Write a c# Function that accept 4 parameters from user and return result of summation and subtracting of two numbers
+            //Console.WriteLine("Enter num 1");
+            //int.TryParse(Console.ReadLine(), out int num1);
+            //Console.WriteLine("Enter num 2");
+            //int.TryParse(Console.ReadLine(), out int num2);
+
+            //int sum , sub;
+            //Sum_Sub(num1, num2, out sum,out sub);
+            //Console.WriteLine($"Sum is { sum}");
+            //Console.WriteLine($"Sub is {sub}");
+
+            #endregion
+
+            #region 4-Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number.
+            //Console.WriteLine(SumOfDigits(220));
+            #endregion
+
+            #region 5-Create a function named "IsPrime", which receives an integer number and retuns true if it is prime, or false if it is not:
+            //Console.WriteLine(IsPrime(8));
+            #endregion
+
+            #region 6-Create a function named MinMaxArray, to return the minimum and maximum values stored in an array, using reference parameters
+            //int[] arr = new int []{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //int min =0, max =0;
+            //MinMaxArray(ref arr, ref min ,ref max);
+            //Console.WriteLine(min);
+            //Console.WriteLine(max);
+            #endregion
+
+            #region 7-Create an iterative (non-recursive) function to calculate the factorial of the number specified as parameter
+            //Console.WriteLine(Factorial(7));
+            #endregion
+
+            #region 8-Create a function named "ChangeChar" to modify a letter in a certain position (0 based) of a string, replacing it with a different letter
+            
+            Console.WriteLine(ChangeChar("sasa", 4, 's'));
+            #endregion
+        }
+    }
+}
